ea9b5b8dd407c94a0875fd441ed19ecf1e0c4c5e
pachikuriii <stakahashi0728@gmail.com>
2022-12-16 21:00:29 +0900

    コンポーネントを１ファイルずつに分けた

---
+++ src/memo-list.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Memo from './memo'
+
+class MemoList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        value: 'Please write an essay about your favorite DOM element.'
+      };
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('An essay was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+        return (
+            <ul>
+               <Memo/>
+            </ul>
+        );
+    }
+  }
+  
+  
+  export default MemoList;
